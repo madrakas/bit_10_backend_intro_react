b149845adfe7fbe70a44f0a813081ed15da5f06c
Arvydas <arvydas.simbelis@gmail.com>
2023-12-13 12:59:07 +0200

    Get types from api

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import { BooksContext } from "./BooksContext";
+
 export default function Book( { book }) {
     const { id, title, author, type, price, img } = book; 
-
+    const { types } = useContext(BooksContext);
     return (
         <div className="book">
             <div className="book__image">
@@ -8,8 +11,8 @@ export default function Book( { book }) {
             </div>
             <div className="book_title">{title}</div>
             <div className="book_author">{author}</div>
-            <div className="book_type">{type}</div>
-            <div className="book_price">{price}</div>
+            <div className="book_type">{types !== null ? types.find(t => t.id === type).title : ''}</div>
+            <div className="book_price">{price} Eur</div>
         </div>
     );
 }
--- src/Components/BooksContext.jsx
+++ src/Components/BooksContext.jsx
@@ -9,20 +9,32 @@ const typesUrl = 'https://in3.dev/knygos/types/';
 export const BooksProvider = ({children}) => {
 
     const [books, setBooks] = useState(null);
+    const [types, setTypes] = useState(null);
 
     useEffect(_ => {
         axios.get(booksUrl)
         .then(res => {
             setBooks(res.data);
-            console.log(res.data);
+            // console.log(res.data);
         })
     }, []);
-    console.log(children);
+    // console.log(children);
+
+    useEffect(_ => {
+        setTimeout(_ => {
+            axios.get(typesUrl)
+            .then(res => {
+            setTypes(res.data);
+            console.log(res.data);
+            })
+        }, 3000)
+        
+    }, []);
 
 
     return (
         <BooksContext.Provider value={{
-            books
+            books, types
         }}>
             {children}
         </BooksContext.Provider>
